09147690ec0c93a236c136a7d3daefb61ee71f18
Simeon Price <simeonaprice@gmail.com>
2019-04-10 08:45:49 +0000

    master: Lab7

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    printf("yeah h
-    ofrti");
+    char alphabet[25] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    printf("%s", alphabet);
+    char word[100];
+    printf("\nEnter your message here: ");
+    scanf(" %[^\n]s", word);
+    printf("%s\n", word);
+    int key;
+    printf("Enter the key here: ");
+    scanf("%d", &key);
+    printf("%d\n", key);
     
-return 0;
+    return 0;
 }
